541cedad2fb1ca212dd919d2d29f410a7c5d06bc
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-10-05 17:43:56 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/OrderedHash.pm
@@ -0,0 +1,174 @@
+package Acme::CPANModules::OrderedHash;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "List of modules that provide ordered hash data type",
+    description => <<'_',
+
+When you ask a Perl's hash for the list of keys, the answer comes back
+unordered. In fact, Perl explicitly randomizes the order of keys it returns
+everytime. The random ordering is a (security) feature, not a bug. However,
+sometimes you want to know the order of insertion. These modules provide you
+with an ordered hash; most of them implement it by recording the order of
+insertion of keys in an additional array.
+
+Other related modules:
+
+<pm:Tie::SortHash> - will automatically sort keys when you call `keys()`,
+`values()`, `each()`. But this module does not maintain insertion order.
+
+_
+    entries => [
+
+        {
+            module => 'Tie::IxHash',
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                tie my %hash, "Tie::IxHash";
+                for (1..$numkeys) { $hash{"key$_"} = $_ }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { delete $hash{"key$_"} }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = keys %hash }
+                }
+            },
+        },
+
+        {
+            module => 'Hash::Ordered',
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                my $hash = Hash::Ordered->new;
+                for (1..$numkeys) { $hash->set("key$_" => $_) }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { $hash->delete("key$_") }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = $hash->keys }
+                }
+            },
+        },
+
+        {
+            module => 'Tie::Hash::Indexed',
+            description => <<'MARKDOWN',
+
+Provides two interfaces: tied hash and OO.
+
+MARKDOWN
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                tie my %hash, "Tie::Hash::Indexed";
+                for (1..$numkeys) { $hash{"key$_"} = $_ }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { delete $hash{"key$_"} }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = keys %hash }
+                }
+            },
+        },
+
+        {
+            module => 'Tie::LLHash',
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                tie my %hash, "Tie::LLHash";
+                for (1..$numkeys) { (tied %hash)->insert("key$_" => $_) }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { delete $hash{"key$_"} }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = keys %hash }
+                }
+            },
+        },
+
+        {
+            module => 'Tie::StoredOrderHash',
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                tie my %hash, "Tie::StoredOrderHash";
+                for (1..$numkeys) { $hash{"key$_"} = $_ }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { delete $hash{"key$_"} }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = keys %hash }
+                }
+            },
+        },
+
+        {
+            module => 'Array::OrdHash',
+            description => <<'_',
+
+Provide something closest to PHP's associative array, where you can refer
+elements by key or by numeric index, and insertion order is remembered.
+
+_
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                my $hash = Array::OrdHash->new;
+                for (1..$numkeys) { $hash->{"key$_"} = $_ }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { delete $hash->{"key$_"} }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = keys %$hash }
+                }
+            },
+        },
+
+        {
+            module => 'List::Unique::DeterministicOrder',
+            description => <<'MARKDOWN',
+
+Provide a list, not hash.
+
+MARKDOWN
+            bench_code => sub {
+                my ($op, $numkeys, $numrep) = @_;
+
+                my $hash = List::Unique::DeterministicOrder->new(data=>[]);
+                for (1..$numkeys) { $hash->push("key$_") }
+
+                if ($op eq 'delete') {
+                    for (1..$numkeys) { $hash->delete("key$_") }
+                } elsif ($op eq 'keys') {
+                    for (1..$numrep) { my @keys = $hash->keys }
+                }
+            },
+        },
+    ],
+
+    bench_datasets => [
+        {name=>'insert 1000 pairs', argv => ['insert', 1000]},
+        {name=>'insert 1000 pairs + delete', argv => ['delete', 1000]},
+        {name=>'insert 1000 pairs + return keys 100 times', argv => ['keys', 1000, 100]},
+        # TODO: iterating
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Acme::CPANModules::HashUtilities>
